31224a7a511777d3f1bec0de1b0e3d042b24a796
Daniel Keiss <daniel.keiss@gmail.com>
2016-09-02 10:58:17 +0200

    first pizza prototype with login functionality

---
+++ src/main/java/pizza/Application.java
@@ -0,0 +1,16 @@
+package pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Daniel Keiss on 01.09.2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/pizza/MvcConfig.java
@@ -0,0 +1,21 @@
+package pizza;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Daniel Keiss on 01.09.2016.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
