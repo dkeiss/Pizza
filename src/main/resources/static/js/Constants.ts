7a53683ff32d93a910a75d3c87335e553c9f5aa4
Sascha <keen_kupege@gmx.net>
2016-10-25 19:10:47 +0200

    admin.html, timepicker, admin.css

--- src/main/resources/static/js/Constants.ts
+++ src/main/resources/static/js/Constants.ts
@@ -8,3 +8,9 @@ class UserManagementSelectors
     public static userTable = ".um-userTable";
     public static userTableRow = ".um-userTable tr";
 }
+
+class AdminOverviewSelectors
+{
+    public static adminTimepicker = ".admin-inputField_time";
+    public static adminTimepickerLabel = ".admin-errorlabel_time";
+}
