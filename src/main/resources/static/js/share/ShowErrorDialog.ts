0117a92d69502f29333ddcfe3ed656bfbdff5112
slora-online <simon.kotowicz@freenet.de>
2016-11-08 22:55:55 +0100

    userOrder - create menu & send menu-error

--- src/main/resources/static/js/share/ShowErrorDialog.ts
+++ src/main/resources/static/js/share/ShowErrorDialog.ts
@@ -4,6 +4,8 @@
 class ShowErrorDialog
 {
     private _container: JQuery = null;
+    private _boxCenter: JQuery = null;
+    private _box: JQuery = null;
     private _title: JQuery = null;
     private _text: JQuery = null;
     private _refreshButton: JQuery = null;
@@ -12,11 +14,36 @@ class ShowErrorDialog
     private _cssShowErrorButton = "core-serverError-showButton";
 
 
-    constructor(xhr: JQueryXHR)
+    constructor(xhr?: JQueryXHR, title?: string, text?:string)
     {
+        this._boxCenter = $(ErrorDialogSelectors.boxCenter);
+        this._box = $(ErrorDialogSelectors.box);
         this._title = $(ErrorDialogSelectors.title);
         this._text = $(ErrorDialogSelectors.text);
 
+        if (xhr)
+        {
+            this.xhrError(xhr);
+        }
+        else if (title || title && text)
+        {
+            this.userError(title, text);
+        }
+        else
+        {
+            this.userError("Unbekannter Fehler");
+        }
+
+        this._container = $(ErrorDialogSelectors.container).addClass(this._cssShowContainer);
+        this._refreshButton = $(ErrorDialogSelectors.refreshButton).addClass(this._cssShowErrorButton).on("click", () => location.reload());
+
+        this._boxCenter.each((index, element) => {
+            $(element).width(this._box.width() + 10);
+        });
+    }
+
+    private xhrError(xhr: JQueryXHR)
+    {
         if (xhr.status == 404)
         {
             this.showServerDialogWithoutDetails();
@@ -25,9 +52,6 @@ class ShowErrorDialog
         {
             this.showServerDialogWithDetails(xhr);
         }
-
-        this._container = $(ErrorDialogSelectors.container).addClass(this._cssShowContainer);
-        this._refreshButton = $(ErrorDialogSelectors.refreshButton).addClass(this._cssShowErrorButton).on("click", () => location.reload());
     }
 
     private showServerDialogWithoutDetails(): void
@@ -46,5 +70,11 @@ class ShowErrorDialog
         return  "<span>Error:</span>   " + xhr.responseJSON.error + "<br />" +
                 "<span>Status:</span>   " + xhr.responseJSON.status + "<br />" +
                 "<span>Message:</span>   " + xhr.responseJSON.message;
+    }
+
+    private userError(title: string, text?: string)
+    {
+        this._title.text(title);
+        if (text) this._text.text(text);
     }
 }
